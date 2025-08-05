6a81dd740bab363552ab2bba379c221d920f3684
daniferdev <danielfernando.dev@gmail.com>
2025-08-05 20:34:08 -0300

    docs: learning notes on comments and docstrings [aula 1]

---
+++ aula1.py
@@ -0,0 +1,12 @@
+"""DocString"""
+"""
+Não é um comentário, mas sim uma string de documentação
+Pode ser usada para descrever o que o código faz, como uma documentação
+"""
+
+# Permite escrever um comentário, que é ignorado pelo Python
+# Comentário de uma linha
+
+print(123)  # Na frente
+# Abaixo
+print(456)
