ff92dba93233a6962b90d42a97018750baff7550
daniferdev <danielfernando.dev@gmail.com>
2025-08-05 22:44:24 -0300

    docs: learning notes and practice exercise on variables and primitive types [aulas 7 e 8]

---
+++ aula7~8.py
@@ -0,0 +1,28 @@
+# Variáveis são usadas para salvar valores na memória do computador.
+# PEP8: inicie o nome de variáveis com letra minúscula e use snake_case.
+# O sinal de = é o operador de atribuição. Atribui valor a variáveis.
+
+# Exemplos: 
+# nome_variavel = expressão
+ex_nome = 'Daniel'  # String
+ex_idade = 22  # Inteiro
+ex_altura = 1.80  # Float
+ex_e_maior = ex_idade > 18  # Boolean
+ex_peso = 80  # Inteiro
+
+# Exercício
+
+nome = 'Daniel'
+sobrenome = 'Fernando'
+idade = 22
+ano_nascimento = 2025 - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.80
+
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
