fd55cbe1fba6ed5b23bd2defc5346ba0f22aa1fe
daniferdev <danielfernando.dev@gmail.com>
2025-08-06 01:43:20 -0300

    docs: learning user input [aula 15]

---
+++ aula15.py
@@ -0,0 +1,7 @@
+# Input User Example
+
+numero_1 = input('Digite o primeiro número: ')
+numero_2 = input('Digite o segundo número: ')
+
+soma = int(numero_1) + int(numero_2)
+print(f'A soma de {numero_1} + {numero_2} é igual a {soma}')
