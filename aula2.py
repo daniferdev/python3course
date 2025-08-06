5fa6164612570921e71443ab77dab269adacc594
daniferdev <danielfernando.dev@gmail.com>
2025-08-05 21:12:42 -0300

    docs: learning print function, sep and end parameter [aula 2]

---
+++ aula2.py
@@ -0,0 +1,8 @@
+# \r\n -> CRLF (Carriage Return + Line Feed)
+# \r -> CR
+# \n -> LF
+
+print(12, 34, 1011, sep="", end='#')
+print(56, 78, 91011, sep="-", end='\n')
+print(10, 25, 9391, sep="-", end='\n')
+print("hello world", sep="-", end='\n')
