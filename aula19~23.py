7ccd9f750ab07468c27bcf8449f82681c0ca8dd7
daniferdev <danielfernando.dev@gmail.com>
2025-08-06 15:02:20 -0300

    docs: learning comparison and logical operators [aulas 19 a 23]

---
+++ aula19~23.py
@@ -0,0 +1,23 @@
+# Operadores de comparação
+# Operadores de comparação são usados para comparar valores e retornar um booleano (True ou False).
+
+# #OP      Significado         Exemplo (True)
+# >       maior               2 > 1
+# >=      maior ou igual      2 >= 2
+# <       menor               1 < 2
+# <=      menor ou igual      2 <= 2
+# ==      igual               'a' == 'a'
+# !=      diferente           'a' != 'b'
+
+maior = 2 > 1
+maior_ou_igual = 2 >= 2
+menor = 1 < 2
+menor_ou_igual = 2 <= 2
+igual = 'a' == 'a'
+diferente = 'a' != 'b'
+print('maior:', maior)
+print('maior_ou_igual:', maior_ou_igual)
+print('menor:', menor)
+print('menor_ou_igual:', menor_ou_igual)
+print('igual:', igual)
+print('diferente:', diferente)
